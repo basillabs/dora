822d0efac41591e56377deff21d713a200a55522
Brett Bukowski <brett.bukowski@airbnb.com>
2017-08-14 20:06:06 -0700

    Renamed from ReactNativeBoilerplate
    - Update displayName in app.json to the new name
    - Delete ios/ and android/ directories
    - Run `react-native eject`
    - Run `react-native link`
    - Rename Registered app component in index.ios.js and index.android.js

--- index.ios.js
+++ index.ios.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 
 import App from './src/App';
 
-AppRegistry.registerComponent('ReactNativeBoilerplate', () => App);
+AppRegistry.registerComponent('Dora', () => App);
