a48131ed37767708c0f722add1b69a6dc3b72603
sangsta <sangsta@users.noreply.github.com>
2017-09-04 21:39:52 -0700

    Rendered detail page text and carousel. (#41)

--- src/containers/DetailScreen.js
+++ src/containers/DetailScreen.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import Card from '../components/Card';
+import { requireImage } from '../constants/Images';
 
 class DetailScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.place.detail,
   });
 
+  renderCarousel(carousel) {
+    // TODO: do it properly with a lightbox or something
+    if (!carousel) {
+      return;
+    }
+    return carousel.map((image) => {
+        return (
+          <Image
+            source={requireImage(image)}
+          />
+        )
+    });
+  }
+
+  renderDetails(details) {
+    return details.map((detail) => {
+      return (
+        <View>
+          <Text>{detail.title}</Text>
+          <Text>{detail.text}</Text>
+          {this.renderCarousel(detail.carousel)}
+        </View>
+      )
+    });
+  }
+
   render() {
-    console.log(this.props);
     const {
       name,
       short_address,
@@ -17,12 +48,14 @@ class DetailScreen extends Component {
       thumbnail,
       details,
     } = this.props.navigation.state.params.place;
+
     return (
       <View style={styles.container}>
         <Card
           imageName={locationImage}
           title={name}
         />
+        {this.renderDetails(details)}
       </View>
     );
   }
