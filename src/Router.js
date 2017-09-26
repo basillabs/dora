348db0d01cd506ad0af097ced12d7d5c626bc4d9
sangsta <sangsta@users.noreply.github.com>
2017-09-25 20:51:57 -0700

    Added lightbox to the carousel. (#49)
    * wip
    
    * working
    
    * tweaks
    
    * Added lightbox to the carousel.
    
    * Got rid of console logs.
    
    * Revert "Clean up merge conflict"
    
    This reverts commit 15680d6edd5c6aae153f4352219c5f4fe73b7fb5, reversing
    changes made to 5d3722862c4309006f6633067e8713a3db623a34.

--- src/Router.js
+++ src/Router.js
@@ -2,10 +2,12 @@ import { StackNavigator } from 'react-navigation';
 
 import IndexScreen from './containers/IndexScreen';
 import DetailScreen from './containers/DetailScreen';
+import CarouselLightbox from './containers/CarouselLightbox';
 
 const routes = {
   Index: { screen: IndexScreen },
   Detail: { screen: DetailScreen },
+  Lightbox: { screen: CarouselLightbox },
 };
 
 const BaseNavigation = StackNavigator(routes);
