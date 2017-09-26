fff1dd5d4ab3b72eb87912003f072462ef9f6ee5
Chris Lee <chris@chrsl.net>
2017-09-25 20:26:00 -0700

    Styling tweaks

--- src/components/Card.js
+++ src/components/Card.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   title: {
     bottom: 20,
     fontSize: 30,
-    fontWeight: '500',
+    fontWeight: '700',
     left: 20,
     lineHeight: 40,
     position: 'absolute',
